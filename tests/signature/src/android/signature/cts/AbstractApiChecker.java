CarbonROM/android_cts

commit adcc353ded74dec98f100d4683636a9c303818e9

Add car APIs
Add Android Car APIs to the list of expected APIs.

Bug: 117814629
Test: atest android.signature.cts.api.AnnotationTest#testAnnotation
Merged-In: I8b8bd1eceb50d489d777fecddf01161c08f33017
Change-Id: I8b8bd1eceb50d489d777fecddf01161c08f33017

Former-commit-id: f838cc917d364faa4a346f4587fadc5b2be52003

tests/signature/api-check/system-annotation/Android.mk
@@ -22,5 +22,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := compatibility-device-util
 LOCAL_SIGNATURE_API_FILES := \
     system-current.api \
     system-removed.api \
+    car-system-current.api \
+    car-system-removed.api \
 
 include $(LOCAL_PATH)/../build_signature_apk.mk

tests/signature/api-check/system-annotation/AndroidTest.xml
@@ -26,6 +26,12 @@
     <target_preparer class="com.android.compatibility.common.tradefed.targetprep.FilePusher">
         <option name="push" value="system-removed.api->/data/local/tmp/signature-test/system-removed.api" />
     </target_preparer>
+    <target_preparer class="com.android.compatibility.common.tradefed.targetprep.FilePusher">
+        <option name="push" value="car-system-current.api->/data/local/tmp/signature-test/car-system-current.api" />
+    </target_preparer>
+    <target_preparer class="com.android.compatibility.common.tradefed.targetprep.FilePusher">
+        <option name="push" value="car-system-removed.api->/data/local/tmp/signature-test/car-system-removed.api" />
+    </target_preparer>
     <target_preparer class="com.android.tradefed.targetprep.suite.SuiteApkInstaller">
         <option name="cleanup-apks" value="true" />
         <option name="test-file-name" value="CtsSystemApiAnnotationTestCases.apk" />
@@ -34,7 +40,7 @@
         <option name="package" value="android.signature.cts.api.system_annotation" />
         <option name="runner" value="repackaged.android.test.InstrumentationTestRunner" />
         <option name="class" value="android.signature.cts.api.AnnotationTest" />
-        <option name="instrumentation-arg" key="expected-api-files" value="system-current.api,system-removed.api" />
+        <option name="instrumentation-arg" key="expected-api-files" value="system-current.api,system-removed.api,car-system-current.api,car-system-removed.api" />
         <option name="instrumentation-arg" key="annotation-for-exact-match" value="android.annotation.SystemApi" />
         <option name="runtime-hint" value="30s" />
     </test>

tests/signature/api/Android.mk
@@ -42,6 +42,8 @@ $(eval $(call build_xml_api_file,system-removed.api,frameworks/base/api/system-r
 $(eval $(call build_xml_api_file,android-test-base-current.api,frameworks/base/test-base/api/android-test-base-current.txt))
 $(eval $(call build_xml_api_file,android-test-mock-current.api,frameworks/base/test-mock/api/android-test-mock-current.txt))
 $(eval $(call build_xml_api_file,android-test-runner-current.api,frameworks/base/test-runner/api/android-test-runner-current.txt))
+$(eval $(call build_xml_api_file,car-system-current.api,packages/services/Car/car-lib/api/system-current.txt))
+$(eval $(call build_xml_api_file,car-system-removed.api,packages/services/Car/car-lib/api/system-removed.txt))
 $(foreach ver,$(PLATFORM_SYSTEMSDK_VERSIONS),\
   $(if $(call math_is_number,$(ver)),\
     $(eval $(call build_xml_api_file,system-$(ver).api,prebuilts/sdk/system-api/$(ver).txt))\

commit 3e01e79185ab67db19c2aa9ecdea76b466a648b8

Fix: AnnotationTest is failing for P-launching devices
AnnotationTest is failing for P-launching devices that do not have
Android Auto APIs. This is a regression caused by adcc353ded74dec98f100d4683636a9c303818e9 [formerly f838cc917d364faa4a346f4587fadc5b2be52003]
that added Android Auto APIs to the expected APIs set.

AbstractApiChecker that the test is inheriting from ensures that all
documented APIs are found in the device under test. That behavior is
required for other signature tests such as
CtsCurrentApiSignatureTestCases, but is not required for this test.
The primary goal of this test is to make sure that the set of APIs (such
as ones annotated with @SystemApi) is not greater than the documented
API set. It is not an error that the former is smaller than the latter.

Fixing the issue by adding a new overridable method 'checkMissingClass'
that can be used for each test to handle the missing class case
differently.

Bug: 122276795
Test: run CtsSystemApiAnnotationTestCases on Pixel 3

Change-Id: Ifd58b10b2c496eb495f5286bf8413302a6c8895f

Former-commit-id: 5668709a901de8f131be767c9a001aa7001a9143

tests/signature/src/android/signature/cts/AbstractApiChecker.java
@@ -62,12 +62,14 @@ public abstract class AbstractApiChecker {
                     .findRequiredClass(classDescription, classProvider);
 
             if (runtimeClass == null) {
-                // No class found, notify the observer according to the class type
-                resultObserver.notifyFailure(FailureType.missing(classDescription),
-                        classDescription.getAbsoluteClassName(),
-                        "Classloader is unable to find " + classDescription
-                                .getAbsoluteClassName());
-
+                // No class found, notify the observer according to the class type,
+                // if missing a class isn't acceptable.
+                if (!allowMissingClass(classDescription)) {
+                    resultObserver.notifyFailure(FailureType.missing(classDescription),
+                            classDescription.getAbsoluteClassName(),
+                            "Classloader is unable to find " + classDescription
+                                    .getAbsoluteClassName());
+                }
                 return null;
             }
 
@@ -104,6 +106,17 @@ public abstract class AbstractApiChecker {
     protected abstract boolean checkClass(JDiffClassDescription classDescription,
             Class<?> runtimeClass);
 
+
+    /**
+     * Checks that a class that exists in the API xml file but that does not exist
+     * in the runtime is allowed or not.
+     *
+     * @param classDescription the class description that is missing.
+     * @return true if missing the class is acceptable.
+     */
+    protected boolean allowMissingClass(JDiffClassDescription classDescription) {
+        return false;
+    }
 
     /**
      * Checks all fields in test class for compliance with the API xml.

tests/signature/src/android/signature/cts/AnnotationChecker.java
@@ -95,6 +95,17 @@ public class AnnotationChecker extends AbstractApiChecker {
     }
 
     @Override
+    protected boolean allowMissingClass(JDiffClassDescription classDescription) {
+        // A class that exist in the API document is not found in the runtime.
+        // This can happen for classes that are optional (e.g. classes for
+        // Android Auto). This, however, should not be considered as a test
+        // failure, because the purpose of this test is to ensure that every
+        // runtime classes found in the device have more annotations than
+        // the documented.
+        return true;
+    }
+
+    @Override
     protected boolean checkClass(JDiffClassDescription classDescription, Class<?> runtimeClass) {
         // remove the class from the set if found
         annotatedClassesMap.remove(runtimeClass.getName());