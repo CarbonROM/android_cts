bluestacks/cts-a16

[RESTRICT AUTOMERGE]: Give tests more control of crash parser behavior
Added a config object instead of a multitude of arguments.
The config object has options for:
	- crash signal (SIGSEGV, SIGABRT, SIGBUS, etc.)
	- whether or not to check the crash address
	- changing the min crash address threshold
	- what process patterns to match against

Bug: 142482078
Test: cts-tradefed run compatibility-common-util-tests
Test: cts-tradefed run cts -m CtsSecurityTestCases -t android.security.cts.StagefrightTest
Change-Id: Id5aec329fe689ce3d43c481b46bfe21550ff9fee

common/util/src/com/android/compatibility/common/util/CrashUtils.java
@@ -17,6 +17,10 @@
 package com.android.compatibility.common.util;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
@@ -59,12 +63,9 @@ public class CrashUtils {
     private static Pattern sAbortMessageCheckPattern =
             Pattern.compile("(?i)Abort message.*?CHECK_");
 
-    /**
-     * returns true if the signal is a segmentation fault or bus error.
-     */
-    public static boolean isSecuritySignal(Crash c) {
-        return c.signal.toLowerCase().matches("sig(segv|bus)");
-    }
+    public static final String SIGSEGV = "SIGSEGV";
+    public static final String SIGBUS = "SIGBUS";
+    public static final String SIGABRT = "SIGABRT";
 
     /**
      * returns the filename of the process.
@@ -78,31 +79,27 @@ public class CrashUtils {
      * Determines if the given input has a {@link com.android.compatibility.common.util.Crash} that
      * should fail an sts test
      *
-     * @param processPatterns list of patterns that match applicable process names
-     * @param checkMinAddr if the minimum fault address should be respected
      * @param crashes list of crashes to check
+     * @param config crash detection configuration object
      * @return if a crash is serious enough to fail an sts test
      */
-    public static boolean securityCrashDetected(
-            List<Crash> crashes, boolean checkMinAddr, Pattern... processPatterns) {
-        return !matchSecurityCrashes(crashes, checkMinAddr, processPatterns).isEmpty();
+    public static boolean securityCrashDetected(List<Crash> crashes, Config config) {
+        return matchSecurityCrashes(crashes, config).size() > 0;
     }
 
     /**
      * Determines which given inputs have a {@link com.android.compatibility.common.util.Crash} that
      * should fail an sts test
      *
-     * @param processPatterns list of patterns that match applicable process names
-     * @param checkMinAddr if the minimum fault address should be respected
      * @param crashes list of crashes to check
+     * @param config crash detection configuration object
      * @return the list of crashes serious enough to fail an sts test
      */
-    public static List<Crash> matchSecurityCrashes(
-            List<Crash> crashes, boolean checkMinAddr, Pattern... processPatterns) {
+    public static List<Crash> matchSecurityCrashes(List<Crash> crashes, Config config) {
         return crashes.stream()
-            .filter(c -> matchesAny(getProcessFileName(c), processPatterns))
-            .filter(c -> isSecuritySignal(c))
-            .filter(c -> !checkMinAddr
+            .filter(c -> matchesAny(getProcessFileName(c), config.getProcessPatterns()))
+            .filter(c -> config.signals.contains(c.signal))
+            .filter(c -> !config.checkMinAddress
                     || c.faultAddress == null || c.faultAddress.compareTo(MIN_CRASH_ADDR) >= 0)
             .collect(Collectors.toList());
     }
@@ -110,7 +107,7 @@ public class CrashUtils {
     /**
      * returns true if the input matches any of the patterns.
      */
-    private static boolean matchesAny(String input, Pattern... patterns) {
+    private static boolean matchesAny(String input, Collection<Pattern> patterns) {
         for (Pattern p : patterns) {
             if (p.matcher(input).matches()) {
                 return true;
@@ -165,5 +162,54 @@ public class CrashUtils {
         }
 
         return crashes;
+    }
+
+    public static class Config {
+        private boolean checkMinAddress = true;
+        private BigInteger minCrashAddress = MIN_CRASH_ADDR;
+        private List<String> signals = Arrays.asList(SIGSEGV, SIGBUS);
+        private List<Pattern> processPatterns = Collections.emptyList();
+
+        public Config setMinAddress(BigInteger minCrashAddress) {
+            this.minCrashAddress = minCrashAddress;
+            return this;
+        }
+
+        public Config checkMinAddress(boolean checkMinAddress) {
+            this.checkMinAddress = checkMinAddress;
+            return this;
+        }
+
+        public Config setSignals(String... signals) {
+            this.signals = Arrays.asList(signals);
+            return this;
+        }
+
+        public Config appendSignals(String... signals) {
+            Collections.addAll(this.signals, signals);
+            return this;
+        }
+
+        public Config setProcessPatterns(String... processPatternStrings) {
+            Pattern[] processPatterns = new Pattern[processPatternStrings.length];
+            for (int i = 0; i < processPatternStrings.length; i++) {
+                processPatterns[i] = Pattern.compile(processPatternStrings[i]);
+            }
+            return setProcessPatterns(processPatterns);
+        }
+
+        public Config setProcessPatterns(Pattern... processPatterns) {
+            this.processPatterns = Arrays.asList(processPatterns);
+            return this;
+        }
+
+        public List<Pattern> getProcessPatterns() {
+            return Collections.unmodifiableList(processPatterns);
+        }
+
+        public Config appendProcessPatterns(Pattern... processPatterns) {
+            Collections.addAll(this.processPatterns, processPatterns);
+            return this;
+        }
     }
 }

CarbonROM/android_cts

Change-Id: Ic8cbecacb71e09bc8e589cdf5d51f3c1c3e12442
Former-commit-id: a3ef63e6f497b81a15e92b7be9a361de0acbdefb

common/util/src/com/android/compatibility/common/util/CrashUtils.java
@@ -17,6 +17,10 @@
 package com.android.compatibility.common.util;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.json.JSONArray;
@@ -61,12 +65,9 @@ public class CrashUtils {
     private static Pattern sAbortMessageCheckPattern =
             Pattern.compile("(?i)Abort message.*?CHECK_");
 
-    /**
-     * returns true if the signal is a segmentation fault or bus error.
-     */
-    public static boolean isSecuritySignal(JSONObject crash) throws JSONException {
-        return crash.getString(SIGNAL).toLowerCase().matches("sig(segv|bus)");
-    }
+    public static final String SIGSEGV = "SIGSEGV";
+    public static final String SIGBUS = "SIGBUS";
+    public static final String SIGABRT = "SIGABRT";
 
     /**
      * returns the filename of the process.
@@ -80,14 +81,12 @@ public class CrashUtils {
      * Determines if the given input has a {@link com.android.compatibility.common.util.Crash} that
      * should fail an sts test
      *
-     * @param processPatterns list of patterns that match applicable process names
-     * @param checkMinAddr if the minimum fault address should be respected
      * @param crashes list of crashes to check
+     * @param config crash detection configuration object
      * @return if a crash is serious enough to fail an sts test
      */
-    public static boolean securityCrashDetected(
-            JSONArray crashes, boolean checkMinAddr, Pattern... processPatterns) {
-        return matchSecurityCrashes(crashes, checkMinAddr, processPatterns).length() > 0;
+    public static boolean securityCrashDetected(JSONArray crashes, Config config) {
+        return matchSecurityCrashes(crashes, config).length() > 0;
     }
 
     public static BigInteger getBigInteger(JSONObject source, String name) throws JSONException {
@@ -106,30 +105,36 @@ public class CrashUtils {
      * Determines which given inputs have a {@link com.android.compatibility.common.util.Crash} that
      * should fail an sts test
      *
-     * @param processPatterns list of patterns that match applicable process names
-     * @param checkMinAddr if the minimum fault address should be respected
      * @param crashes list of crashes to check
+     * @param config crash detection configuration object
      * @return the list of crashes serious enough to fail an sts test
      */
-    public static JSONArray matchSecurityCrashes(
-            JSONArray crashes, boolean checkMinAddr, Pattern... processPatterns) {
+    public static JSONArray matchSecurityCrashes(JSONArray crashes, Config config) {
         JSONArray securityCrashes = new JSONArray();
         for (int i = 0; i < crashes.length(); i++) {
             try {
                 JSONObject crash = crashes.getJSONObject(i);
-                if (!matchesAny(getProcessFileName(crash), processPatterns)) {
+                // match process patterns
+                if (!matchesAny(getProcessFileName(crash), config.processPatterns)) {
                     continue;
                 }
-                if (!isSecuritySignal(crash)) {
+
+                // match signal
+                String crashSignal = crash.getString(SIGNAL);
+                if (!config.signals.contains(crashSignal)) {
                     continue;
                 }
-                BigInteger faultAddress = getBigInteger(crash, FAULT_ADDRESS);
-                if (checkMinAddr && faultAddress != null
-                        && faultAddress.compareTo(MIN_CRASH_ADDR) < 0) {
-                    continue;
+
+                // if check specified, reject crash if address is unlikely to be security-related
+                if (config.checkMinAddress) {
+                    BigInteger faultAddress = getBigInteger(crash, FAULT_ADDRESS);
+                    if (faultAddress != null
+                            && faultAddress.compareTo(config.minCrashAddress) < 0) {
+                        continue;
+                    }
                 }
                 securityCrashes.put(crash);
-            } catch (JSONException | NullPointerException e) {}
+            } catch (JSONException e) {}
         }
         return securityCrashes;
     }
@@ -137,7 +142,7 @@ public class CrashUtils {
     /**
      * returns true if the input matches any of the patterns.
      */
-    private static boolean matchesAny(String input, Pattern... patterns) {
+    private static boolean matchesAny(String input, Collection<Pattern> patterns) {
         for (Pattern p : patterns) {
             if (p.matcher(input).matches()) {
                 return true;
@@ -195,5 +200,54 @@ public class CrashUtils {
             }
         }
         return crashes;
+    }
+
+    public static class Config {
+        private boolean checkMinAddress = true;
+        private BigInteger minCrashAddress = MIN_CRASH_ADDR;
+        private List<String> signals = Arrays.asList(SIGSEGV, SIGBUS);
+        private List<Pattern> processPatterns = Collections.emptyList();
+
+        public Config setMinAddress(BigInteger minCrashAddress) {
+            this.minCrashAddress = minCrashAddress;
+            return this;
+        }
+
+        public Config checkMinAddress(boolean checkMinAddress) {
+            this.checkMinAddress = checkMinAddress;
+            return this;
+        }
+
+        public Config setSignals(String... signals) {
+            this.signals = Arrays.asList(signals);
+            return this;
+        }
+
+        public Config appendSignals(String... signals) {
+            Collections.addAll(this.signals, signals);
+            return this;
+        }
+
+        public Config setProcessPatterns(String... processPatternStrings) {
+            Pattern[] processPatterns = new Pattern[processPatternStrings.length];
+            for (int i = 0; i < processPatternStrings.length; i++) {
+                processPatterns[i] = Pattern.compile(processPatternStrings[i]);
+            }
+            return setProcessPatterns(processPatterns);
+        }
+
+        public Config setProcessPatterns(Pattern... processPatterns) {
+            this.processPatterns = Arrays.asList(processPatterns);
+            return this;
+        }
+
+        public List<Pattern> getProcessPatterns() {
+            return Collections.unmodifiableList(processPatterns);
+        }
+
+        public Config appendProcessPatterns(Pattern... processPatterns) {
+            Collections.addAll(this.processPatterns, processPatterns);
+            return this;
+        }
     }
 }