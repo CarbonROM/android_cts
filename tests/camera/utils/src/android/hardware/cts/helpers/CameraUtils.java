bluestacks/cts-a16

Check correct camera if it is external
In the presence of non-regular Camera2 devices (monochrome
or logicalMultiCamera), the number of devices available to
Camera1 clients is different from the number of devices
available to Camera2 clients. This difference causes
CameraUtils.isExternal() to sometimes check the wrong camera.

Bug: 168509179
Test: android.hardware.cts.CameraTest#testLockUnlock
    android.hardware.cts.CameraTest#testRecordingHint
    android.hardware.cts.CameraTest#testParameters

Change-Id: I419215f135eb03992860146ec88eca9650a26bf7

tests/camera/utils/src/android/hardware/cts/helpers/CameraUtils.java
@@ -20,13 +20,17 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.cts.helpers.StaticMetadata;
 
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * Utility class containing helper functions for the Camera CTS tests.
  */
 public class CameraUtils {
+    private static final float FOCAL_LENGTH_TOLERANCE = .01f;
 
     /**
      * Returns {@code true} if this device only supports {@code LEGACY} mode operation in the
@@ -59,6 +63,80 @@ public class CameraUtils {
     }
 
     /**
+     * Returns {@code true} if the Camera.Parameter and Camera.Info arguments describe a similar
+     * camera as the CameraCharacteristics.
+     *
+     * @param params Camera.Parameters to use for matching.
+     * @param info Camera.CameraInfo to use for matching.
+     * @param ch CameraCharacteristics to use for matching.
+     * @return {@code true} if the arguments describe similar camera devices.
+     */
+    public static boolean matchParametersToCharacteristics(Camera.Parameters params,
+            Camera.CameraInfo info, CameraCharacteristics ch) {
+        Integer facing = ch.get(CameraCharacteristics.LENS_FACING);
+        switch (facing.intValue()) {
+            case CameraMetadata.LENS_FACING_EXTERNAL:
+            case CameraMetadata.LENS_FACING_FRONT:
+                if (info.facing != Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    return false;
+                }
+                break;
+            case CameraMetadata.LENS_FACING_BACK:
+                if (info.facing != Camera.CameraInfo.CAMERA_FACING_BACK) {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+
+        Integer orientation = ch.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        if (orientation.intValue() != info.orientation) {
+            return false;
+        }
+
+        StaticMetadata staticMeta = new StaticMetadata(ch);
+        boolean legacyHasFlash = params.getSupportedFlashModes() != null;
+        if (staticMeta.hasFlash() != legacyHasFlash) {
+            return false;
+        }
+
+        boolean isExternal = (ch.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) ==
+                CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_EXTERNAL);
+        boolean hasValidMinFocusDistance = staticMeta.areKeysAvailable(
+                CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE);
+        boolean fixedFocusExternal = isExternal && !hasValidMinFocusDistance;
+        boolean hasFocuser = staticMeta.hasFocuser() && !fixedFocusExternal;
+        List<String> legacyFocusModes = params.getSupportedFocusModes();
+        boolean legacyHasFocuser = !((legacyFocusModes.size() == 1) &&
+                (legacyFocusModes.contains(Camera.Parameters.FOCUS_MODE_FIXED)));
+        if (hasFocuser != legacyHasFocuser) {
+            return false;
+        }
+
+        if (staticMeta.isVideoStabilizationSupported() != params.isVideoStabilizationSupported()) {
+            return false;
+        }
+
+        float legacyFocalLength = params.getFocalLength();
+        if (ch.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS) != null) {
+            float [] focalLengths = staticMeta.getAvailableFocalLengthsChecked();
+            boolean found = false;
+            for (float focalLength : focalLengths) {
+                if (Math.abs(focalLength - legacyFocalLength) <= FOCAL_LENGTH_TOLERANCE) {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        } else if (legacyFocalLength != -1.0f) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
      * Returns {@code true} if this device only supports {@code EXTERNAL} mode operation in the
      * Camera2 API for the given camera ID.
      *
@@ -68,9 +146,37 @@ public class CameraUtils {
      */
     public static boolean isExternal(Context context, int cameraId) throws Exception {
         CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
-        String cameraIdStr = manager.getCameraIdList()[cameraId];
+
+        Camera camera = null;
+        Camera.Parameters params = null;
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        try {
+            Camera.getCameraInfo(cameraId, info);
+            camera = Camera.open(cameraId);
+            params = camera.getParameters();
+        } finally {
+            if (camera != null) {
+                camera.release();
+            }
+        }
+
+        String [] cameraIdList = manager.getCameraIdList();
         CameraCharacteristics characteristics =
-                manager.getCameraCharacteristics(cameraIdStr);
+                manager.getCameraCharacteristics(cameraIdList[cameraId]);
+
+        if (!matchParametersToCharacteristics(params, info, characteristics)) {
+            boolean found = false;
+            for (String id : cameraIdList) {
+                characteristics = manager.getCameraCharacteristics(id);
+                if (matchParametersToCharacteristics(params, info, characteristics)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
 
         return characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) ==
                 CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_EXTERNAL;

CarbonROM/android_cts

tests/camera/utils/src/android/hardware/cts/helpers/CameraUtils.java
@@ -20,17 +20,21 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.cts.helpers.StaticMetadata;
 import android.os.Bundle;
 
 import androidx.test.InstrumentationRegistry;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.IntStream;
 
 /**
  * Utility class containing helper functions for the Camera CTS tests.
  */
 public class CameraUtils {
+    private static final float FOCAL_LENGTH_TOLERANCE = .01f;
 
     private static final String CAMERA_ID_INSTR_ARG_KEY = "camera-id";
     private static final Bundle mBundle = InstrumentationRegistry.getArguments();
@@ -67,6 +71,80 @@ public class CameraUtils {
     }
 
     /**
+     * Returns {@code true} if the Camera.Parameter and Camera.Info arguments describe a similar
+     * camera as the CameraCharacteristics.
+     *
+     * @param params Camera.Parameters to use for matching.
+     * @param info Camera.CameraInfo to use for matching.
+     * @param ch CameraCharacteristics to use for matching.
+     * @return {@code true} if the arguments describe similar camera devices.
+     */
+    public static boolean matchParametersToCharacteristics(Camera.Parameters params,
+            Camera.CameraInfo info, CameraCharacteristics ch) {
+        Integer facing = ch.get(CameraCharacteristics.LENS_FACING);
+        switch (facing.intValue()) {
+            case CameraMetadata.LENS_FACING_EXTERNAL:
+            case CameraMetadata.LENS_FACING_FRONT:
+                if (info.facing != Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    return false;
+                }
+                break;
+            case CameraMetadata.LENS_FACING_BACK:
+                if (info.facing != Camera.CameraInfo.CAMERA_FACING_BACK) {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+        }
+
+        Integer orientation = ch.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        if (orientation.intValue() != info.orientation) {
+            return false;
+        }
+
+        StaticMetadata staticMeta = new StaticMetadata(ch);
+        boolean legacyHasFlash = params.getSupportedFlashModes() != null;
+        if (staticMeta.hasFlash() != legacyHasFlash) {
+            return false;
+        }
+
+        boolean isExternal = (ch.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) ==
+                CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_EXTERNAL);
+        boolean hasValidMinFocusDistance = staticMeta.areKeysAvailable(
+                CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE);
+        boolean fixedFocusExternal = isExternal && !hasValidMinFocusDistance;
+        boolean hasFocuser = staticMeta.hasFocuser() && !fixedFocusExternal;
+        List<String> legacyFocusModes = params.getSupportedFocusModes();
+        boolean legacyHasFocuser = !((legacyFocusModes.size() == 1) &&
+                (legacyFocusModes.contains(Camera.Parameters.FOCUS_MODE_FIXED)));
+        if (hasFocuser != legacyHasFocuser) {
+            return false;
+        }
+
+        if (staticMeta.isVideoStabilizationSupported() != params.isVideoStabilizationSupported()) {
+            return false;
+        }
+
+        float legacyFocalLength = params.getFocalLength();
+        if (ch.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS) != null) {
+            float [] focalLengths = staticMeta.getAvailableFocalLengthsChecked();
+            boolean found = false;
+            for (float focalLength : focalLengths) {
+                if (Math.abs(focalLength - legacyFocalLength) <= FOCAL_LENGTH_TOLERANCE) {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        } else if (legacyFocalLength != -1.0f) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
      * Returns {@code true} if this device only supports {@code EXTERNAL} mode operation in the
      * Camera2 API for the given camera ID.
      *
@@ -76,9 +154,37 @@ public class CameraUtils {
      */
     public static boolean isExternal(Context context, int cameraId) throws Exception {
         CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
-        String cameraIdStr = manager.getCameraIdListNoLazy()[cameraId];
+
+        Camera camera = null;
+        Camera.Parameters params = null;
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        try {
+            Camera.getCameraInfo(cameraId, info);
+            camera = Camera.open(cameraId);
+            params = camera.getParameters();
+        } finally {
+            if (camera != null) {
+                camera.release();
+            }
+        }
+
+        String [] cameraIdList = manager.getCameraIdListNoLazy();
         CameraCharacteristics characteristics =
-                manager.getCameraCharacteristics(cameraIdStr);
+                manager.getCameraCharacteristics(cameraIdList[cameraId]);
+
+        if (!matchParametersToCharacteristics(params, info, characteristics)) {
+            boolean found = false;
+            for (String id : cameraIdList) {
+                characteristics = manager.getCameraCharacteristics(id);
+                if (matchParametersToCharacteristics(params, info, characteristics)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
 
         return characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) ==
                 CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_EXTERNAL;