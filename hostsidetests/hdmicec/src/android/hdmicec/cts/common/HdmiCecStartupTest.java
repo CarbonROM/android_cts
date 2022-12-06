bluestacks/cts-a16

CTS: Updating expected hdmi-cec commands
For hdmi-cec test case, cectVerifyStartupMessages_Cec14b allowedMessages list needs an update. REPORT_POWER_STATUS is an expected command from a device to display the device. HDMI CEC 2.0 onwards, the source device sends REPORT_POWER_STATUS is sent by source device to the TV whenever it enters the active state. CecOperand enum is added with MENU_STATUS.

Bug: 241711235
Test: HdmiCecStartupTest
Change-Id: I1af11ae0ee9d0a9b1629d43f0028ece5336a3f80

hostsidetests/hdmicec/src/android/hdmicec/cts/common/HdmiCecStartupTest.java
@@ -66,7 +66,7 @@ public final class HdmiCecStartupTest extends BaseHdmiCecCtsTest {
                         CecOperand.DEVICE_VENDOR_ID, CecOperand.GIVE_POWER_STATUS,
                         CecOperand.GET_MENU_LANGUAGE, CecOperand.ACTIVE_SOURCE,
                         CecOperand.REQUEST_ACTIVE_SOURCE, CecOperand.GIVE_PHYSICAL_ADDRESS,
-                        CecOperand.GIVE_SYSTEM_AUDIO_MODE_STATUS));
+                        CecOperand.REPORT_POWER_STATUS, CecOperand.GIVE_SYSTEM_AUDIO_MODE_STATUS));
         allowedMessages.addAll(expectedMessages);
 
         device.reboot();

CarbonROM/android_cts

(cherry picked from commit d124f43816a5ed6ada32f8e373c26f4d4e7ec5c9)
(cherry picked from commit 14c1fe1561bb42cd893680e7141b8c6bf9b7b592)
Former-commit-id: 635d1a3403f554687ba32da983eda2f03bfc9b2f

hostsidetests/hdmicec/src/android/hdmicec/cts/common/HdmiCecStartupTest.java
@@ -66,7 +66,7 @@ public final class HdmiCecStartupTest extends BaseHdmiCecCtsTest {
                         CecOperand.DEVICE_VENDOR_ID, CecOperand.GIVE_POWER_STATUS,
                         CecOperand.GET_MENU_LANGUAGE, CecOperand.ACTIVE_SOURCE,
                         CecOperand.REQUEST_ACTIVE_SOURCE, CecOperand.GIVE_PHYSICAL_ADDRESS,
-                        CecOperand.GIVE_SYSTEM_AUDIO_MODE_STATUS));
+                        CecOperand.GIVE_SYSTEM_AUDIO_MODE_STATUS, CecOperand.REPORT_POWER_STATUS));
         allowedMessages.addAll(expectedMessages);
 
         device.reboot();